af5235e79b0de6674262bdcd2b531bfee2c69f9e
alessstft <alesssftf@gmail.com>
2025-06-13 22:22:47 +0300

    Update CarLogic.cs

--- Domain/CarLogic.cs
+++ Domain/CarLogic.cs
@@ -3,7 +3,6 @@ namespace LegoCarStoreEF.Domain
     public class CarLogic
     {
         public bool IsValidPrice(decimal price) => price >= 0 && price < 1000;
-
         public string FormatDisplay(LegoCarBase car)
         {
             return $"[{car.GetCategory()}] {car.Name} - {car.Price:C}";
