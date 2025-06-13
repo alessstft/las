9341a4e12d38095e97de9bfc74464e3f829fdde9
alessstft <alesssftf@gmail.com>
2025-06-13 22:22:59 +0300

    Update LegoCarBase.cs

--- Domain/LegoCarBase.cs
+++ Domain/LegoCarBase.cs
@@ -2,6 +2,7 @@ namespace LegoCarStoreEF.Domain
 {
     public abstract class LegoCarBase
     {
+        
         public string Name { get; set; }
         public decimal Price { get; set; }
         public abstract string GetCategory();
